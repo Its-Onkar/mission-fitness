011ac87f0134b324fb4e61a332d2ba3ed67b9d66
ramanbraham <ramanpreet@tickmark.io>
2025-07-29 13:53:37 +0530

    :feat

--- src/Schema/diet.schema.js
+++ src/Schema/diet.schema.js
@@ -24,7 +24,7 @@ const dietSchema = new mongoose.Schema(
 
     status: {
       type: String,
-      enum: ["Active", "Inactive"],
+      enum: ["completed","pending"],
       default: "Active",
     },
   },
